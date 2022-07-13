376c7cd73e737542feab2684711e9e00f60eaa18
Joevin <95735030+nnnvnnn@users.noreply.github.com>
2022-07-13 15:41:51 +0800

    Add files via upload

---
+++ templates/js/scripts.js
@@ -0,0 +1,12 @@
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const sideNav = document.body.querySelector('#sideNav');
+    if (sideNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#sideNav',
+            offset: 74,
+        });
+        
+    }
+};
